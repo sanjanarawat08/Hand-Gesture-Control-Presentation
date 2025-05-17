61735b01f5bcdfc0e69dd9ab821455b6910b3239
Sanjana Rawat <siya08rawat@gmail.com>
2025-05-17 21:51:29 +0530

    Update Gesture.py

--- Gesture.py
+++ Gesture.py
@@ -1,53 +1,50 @@
-from pickle import FALSE 
-import cv2 as cv                                    #to capture slide and webcam
-import os                                           #to add the folder with current program
-from cvzone.HandTrackingModule import HandDetector  #to detect hands movement and gestures
+from pickle import FALSE
+import cv2 as cv
+import os
+from cvzone.HandTrackingModule import HandDetector
 import numpy as np
-from pyparsing import annotations #to calculate co-ordinates
-import speech_recognition as sr                     #to implement voice controls
+from pyparsing import annotations
+import speech_recognition as sr  # Add speech recognition
 
 
-# Variables 
- 
-width, height = 1000, 600
+# variables
+
+width, height=1000, 800
 folderPath = r"C:\Users\hp\Desktop\New folder"
 
 
 # camera setup
 
-cap = cv.VideoCapture(0)
+cap= cv.VideoCapture(0)
 
-cap.set(3, width)
-cap.set(4, height)
+cap.set(3,width)
+cap.set(4,height)
 
-# get the list of presentation images
+# get the list of presentation images 
 
-path_images = sorted(os.listdir(folderPath), key=len)
+path_images = sorted(os.listdir(folderPath),key=len)
 
-#variables
-imgNumber = 0
-hs, ws = int(120 * 1.8), int(213 * 1.8)
-gestureThreshold = 350
-buttonpressed = False
-buttoncounter = 0
-delaycounter = 20
 
-#variables used in drawing and erasing 
-annotations = [[]]
-annotationNumber = -1
-annotationStart = False
-voiceAnnotation = False
+# variables
+imgNumber= 0
+hs, ws=int(120*1.5),int(213*1.5)
+gestureThreshold= 400
+buttonpressed=False # used for button delay 
+buttoncounter=0
+delaycounter=20
+annotations=[[]]
+annotationNumber= -1
+annotationStart=False
 
-#implement zoom functioning
 zoomScale = 1.0
 zoomCenter = (width // 2, height // 2)
 initialDistance = None
 
-# function to detect hands
-detector = HandDetector(detectionCon=0.85, maxHands=2)
+# hand detector
+detector= HandDetector(detectionCon=0.8,maxHands=2)
 
 
-#function to get voice recognition
+# listen to voice commands
 def get_voice_command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -63,112 +60,126 @@ def get_voice_command():
             return ""
 
 
-#implment the loop when hands are detected
 while True:
+    # import images
 
-    success, img = cap.read()
-    img = cv.flip(img, 1)
+    success, img= cap.read()
+    img=cv.flip(img,1)
 
-    pathFullImage = os.path.join(folderPath, path_images[imgNumber])    #retrun the list of images in sorted order from given folder path
-    imgCurrent = cv.imread(pathFullImage)
+    pathFullImage= os.path.join(folderPath,path_images[imgNumber])
+    imgCurrent=cv.imread(pathFullImage)
 
-    hands, img = detector.findHands(img)    #detect hands
+    hands,img=detector.findHands(img)
+    cv.line(img,(0,gestureThreshold),(width,gestureThreshold),(255,0,0),5)
 
-    cv.line(img, (0, gestureThreshold), (width, gestureThreshold), (255, 0, 0), 5) #threshold line to prevent disturbance
-
-    if hands and not buttonpressed:
-        hand = hands[0]
-        fingers = detector.fingersUp(hand)  #detect fingertips and return a list 
-
-        #drawing portions
-        #  
-        x, y = hand['center']
-        lmList = hand['lmList']
-
-        xVal = int(np.interp(lmList[8][0], [0, width], [0, width])) #represent index finger
-        yVal = int(np.interp(lmList[8][1], [150, height - 150], [0, height])) #represent middle finger
-        indexFinger = xVal, yVal
-
-        # When hands are above the threshold line
-        if y <= gestureThreshold:
-
-            # Gesture 01 Move to previous slide
-            if fingers == [1, 0, 0, 0, 0] and imgNumber > 0:
-                buttonpressed = True
-
-                #To remove all drawings when you move to next page
-                annotations = [[]]
-                annotationNumber = -1
-                annotationStart = False
-                imgNumber -= 1
+    if hands and buttonpressed==False:
+        hand= hands[0]
+        fingers= detector.fingersUp(hand)
+        x,y=hand['center']
+        lmList= hand['lmList'] # landmark list
 
 
-            # Gesture 02 Move to next slide
-            if fingers == [0, 0, 0, 0, 1] and imgNumber < len(path_images) - 1:
-                buttonpressed = True
-                annotations = [[]]
-                annotationNumber = -1
-                annotationStart = False
-                imgNumber += 1
-
-            if fingers == [0, 1, 0, 0, 1]:  # Gesture 03 Exit the window
-                break
+        # constraits value for easy movement (drawing)
         
-        # Gesture 04 Show pointer used for drawing 
-        if fingers == [0, 1, 1, 0, 0]:
-            cv.circle(imgCurrent, indexFinger, 22, (0, 0, 255), cv.FILLED)
+        xVal= int(np.interp(lmList[8][0],[0,width],[0,width]))
+        yVal= int(np.interp(lmList[8][1],[0,height],[0,height]))
 
-        # Gesture 05 Draw with Indexfinger 
-        if fingers == [0, 1, 0, 0, 0]:
-            if not annotationStart:
-                annotationStart = True
-                annotationNumber += 1
-                annotations.append([])
+        indexFinger= xVal,yVal
 
-            cv.circle(imgCurrent, indexFinger, 22, (0, 0, 255), cv.FILLED)
+        
+
+        if y<=gestureThreshold: #if hand is align with my face
+
+            # gesture 1  left
+            if fingers== [1,0,0,0,0]:
+    
+                if imgNumber>0:
+                    buttonpressed= True
+
+                    annotations=[[]]
+                    annotationNumber= -1
+                    annotationStart=False
+
+                    imgNumber-=1
+                    
+
+            #gesture 2  right
+            if fingers== [0,0,0,0,1]:
+                if imgNumber<len(path_images)-1:
+                    buttonpressed= True
+
+                    annotations=[[]]
+                    annotationNumber= -1
+                    annotationStart=False
+        
+                    imgNumber+=1
+                    
+            #gesture close
+            if fingers==[0,1,0,0,1]:
+                break
+            
+        # Gesture 3 show pointer
+        if fingers== [0,1,1,0,0]:
+            cv.circle(imgCurrent,indexFinger,22,(0,0,255),cv.FILLED)
+        
+        # gesture 4 draw 
+        if fingers==[0,1,0,0,0]:
+            if annotationStart is False:
+                annotationStart=True
+                annotationNumber+=1
+                annotations.append([]) # if not added give error as no list will be there to append
+
+            cv.circle(imgCurrent,indexFinger,22,(0,0,255),cv.FILLED)
             annotations[annotationNumber].append(indexFinger)
-
         else:
-            annotationStart = False
+            annotationStart=False    
 
-        # Gesture 06 Erase last stroke 
-        if fingers == [0, 1, 1, 1, 0]:
+        # gesture 5 erase
+        if fingers==[0,1,1,1,0]:
             if annotations:
                 annotations.pop(-1)
-                annotationNumber -= 1
-                buttonpressed = True
-
-        #Gesture 07 Zoom current slide
-        if fingers == [1, 1, 0, 0, 0]:  
-            x1, y1 = lmList[4][0], lmList[4][1]
-            x2, y2 = lmList[8][0], lmList[8][1]
+                annotationNumber-=1
+                buttonpressed=True
+        
+        # Zoom gesture: Thumb and Index Up
+        if fingers == [1, 1, 0, 0, 0]:
+            x1, y1 = lmList[4][0], lmList[4][1]  # Thumb tip
+            x2, y2 = lmList[8][0], lmList[8][1]  # Index tip
             currentDistance = np.hypot(x2 - x1, y2 - y1)
 
             if initialDistance is None:
                 initialDistance = currentDistance
 
             zoomFactor = currentDistance / initialDistance
-            zoomScale = np.clip(zoomFactor, 0.5, 2.0)
-            zoomCenter = ((x1 + x2) // 2, (y1 + y2) // 2)
-
+            zoomScale = np.clip(zoomFactor, 0.5, 2.0)  # Zoom between 0.5x and 2x
         else:
-            initialDistance = None
-
+            initialDistance = None  # Reset when gesture not held
+        
     else:
-        annotationStart = False
+        annotationStart=False 
 
-    # process to generate a delay when traversing slides  
+    #button pressed iterations
     if buttonpressed:
-        buttoncounter += 1
-        if buttoncounter > delaycounter:
-            buttoncounter = 0
-            buttonpressed = False
+        buttoncounter+=1
+        if buttoncounter>delaycounter:
+            buttoncounter=0
+            buttonpressed=False
 
-    #draw line when and add it to annottion list  
-    for i in range(len(annotations)):
+    # draw a line freely
+    for i in range (len(annotations)):
         for j in range(len(annotations[i])):
-            if j != 0:
-                cv.line(imgCurrent, annotations[i][j - 1], annotations[i][j], (0, 125, 23), 12)
+            if j!=0:
+                cv.line(imgCurrent,annotations[i][j-1],annotations[i][j],(0,125,23),12)
+
+    # resize the slide
+    imgCurrent = cv.resize(imgCurrent, (width, height))
+    
+    #adding webcam on slide
+    
+    imgSmall=cv.resize(img,(ws,hs))
+    h,w,_ =imgCurrent.shape
+
+    imgCurrent[0:hs, w-ws:w]= imgSmall
 
 
     #Resize imgCurrent based on zoomScale
@@ -200,19 +211,39 @@ while True:
 
     imgCurrent = finalZoom
 
-    #Important windows 
-    cv.imshow("Image", img)
-    cv.imshow("Slide", imgCurrent)
 
-    key = cv.waitKey(1) #waiting key
+    # Add to your while loop after imgCurrent is ready
+    # Display Slide Number
+    cv.putText(imgCurrent, f"Slide: {imgNumber + 1}/{len(path_images)}", (40, 60), 
+            cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+    # Zoom Display
+    cv.putText(imgCurrent, f"Zoom: {zoomScale:.1f}x", (40, 110), 
+            cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+
+    # Drawing Mode Indicator
+    if annotationStart:
+        cv.putText(imgCurrent, "Drawing...", (40, 160), 
+                cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+
+    # Voice command prompt
+    cv.putText(imgCurrent, "Press 'V' for Voice Command", (40, height - 40), 
+            cv.FONT_HERSHEY_SIMPLEX, 0.8, (100, 255, 100), 2)
 
 
-    #break the loop
-    if key == ord('q'):
+
+    cv.imshow("Image",img)
+    cv.imshow("Slide",imgCurrent)
+
+    
+    key=cv.waitKey(1)
+
+    if key== ord('q'):
         break
 
-    # acessing voice controls with by pressing 'v'    
-     if key == ord('v'):
+
+    # Voice command trigger on pressing 'v'
+    if key == ord('v'):
         command = get_voice_command()
         if "next" in command or "next slide" in command or "move to the next slide" in command or "next page" in command :
             if imgNumber < len(path_images) - 1:
@@ -239,7 +270,7 @@ while True:
                 annotations.pop(annotationNumber)
                 annotationNumber -= 1
                 print("Undo last stroke")
-       elif "erase" in command or "clear" in command or "clear screen" in command or "remove" in command:
+        elif "erase" in command or "clear" in command or "clear screen" in command or "remove" in command:
             annotations = [[]]
             annotationNumber = -1
             annotationStart = False
